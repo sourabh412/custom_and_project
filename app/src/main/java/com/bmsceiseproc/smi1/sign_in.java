8c3085e049b069af1020ff545e5adb0ebb341b73
sourabh <Hiremathsourabh12@gmail.com>
2022-08-17 22:53:06 +0530

    2

--- app/src/main/java/com/bmsceiseproc/smi1/sign_in.java
+++ app/src/main/java/com/bmsceiseproc/smi1/sign_in.java
@@ -2,13 +2,39 @@ package com.bmsceiseproc.smi1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class sign_in extends AppCompatActivity {
 
+    TextView signin_toggle;
+    Button signin_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
+
+        signin_button = findViewById(R.id.signin_button);
+        signin_toggle = findViewById(R.id.signin_toggle);
+
+        signin_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(sign_in.this, dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        signin_toggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(sign_in.this, sign_up.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/bmsceiseproc/smi1/sign_up.java
+++ app/src/main/java/com/bmsceiseproc/smi1/sign_up.java
@@ -2,13 +2,41 @@ package com.bmsceiseproc.smi1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class sign_up extends AppCompatActivity {
+
+    TextView signup_toggle;
+    Button signup_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+
+        signup_button = findViewById(R.id.signup_button);
+        signup_toggle = findViewById(R.id.signup_toggle);
+
+        signup_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(sign_up.this, dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        signup_toggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(sign_up.this, sign_in.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
