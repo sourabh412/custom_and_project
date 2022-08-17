2075c41ef1298005ca7569f2fcf9d7d49e8345d8
sourabh <Hiremathsourabh12@gmail.com>
2022-08-17 18:54:49 +0530

    first commit

---
+++ app/src/main/java/com/bmsceiseproc/smi1/sign_up.java
@@ -0,0 +1,14 @@
+package com.bmsceiseproc.smi1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sign_up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+    }
+}
